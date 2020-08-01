861c6214dbdc95bc97f2cf29450b840afb55e6a0
yasminralberto <67976210+yasminralberto@users.noreply.github.com>
2020-08-01 18:20:38 -0500

    Add files via upload

---
+++ Yasmin_A_FunctionExercise.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+import Cocoa
+
+func equation (answer : Int) {
+    print("2 times 2 is \(answer)")
+    
+}
+equation(answer: 2*2)
+
+func printequation(answer: Int, Equation: String){
+    print("\(Equation) is \(answer)")
+}
+
+printequation(answer: (4*4) , Equation: "4 times 4")
